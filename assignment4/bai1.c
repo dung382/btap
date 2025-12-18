ba09a70ee11b97914040fa0b48dfe62fdd2e888a
dung382 <dungdinh300305@gmail.com>
2025-12-18 14:43:55 +0700

    sua loi

--- assignment4/bai1.c
+++ assignment4/bai1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-void swap(int &evenNumber, int &oddNumber) {
-    int temp = evenNumber;
-    evenNumber = oddNumber;
-    oddNumber = temp;
+// khong dung duoc pass by ref vi trong c khong ho tro 
+void swap(int *evenNumber, int *oddNumber) {
+    int temp = *evenNumber;
+    *evenNumber = *oddNumber;
+    *oddNumber = temp;
 }		
 int main() {
     int evenNumber = 2;
-    int oddNumber = 3;
+    int oddNumber   = 3;
     printf("evenNumber: %d, oddNumber : %d\n", evenNumber, oddNumber);
-    swap(evenNumber, oddNumber);
+    swap(&evenNumber, &oddNumber);
     printf("evenNumber: %d, oddNumber : %d\n", evenNumber, oddNumber);
     return 0;
 }
